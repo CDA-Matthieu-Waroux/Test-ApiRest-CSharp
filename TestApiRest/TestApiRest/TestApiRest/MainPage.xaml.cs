419c234e064c281370e5a5f51b7ae7f389eb0ca7
Matthieu Waroux <Matthieu_Waroux59@hotmail.com>
2019-07-19 10:45:16 +0200

    Apport de la solution

---
+++ TestApiRest/TestApiRest/TestApiRest/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestApiRest
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            
+		}
+
+        public void Frankouille()
+        {
+
+            string url = "https://api.coinmarketcap.com/v1/ticker/";
+
+            using (var client = new WebClient())
+            {
+                var json = client.DownloadString(url);
+                var donnee = JsonConvert.DeserializeObject<List<RootObject>>(json);
+               
+                foreach(var frankencouille in donnee)
+                {
+                    LabelNom.Text = frankencouille.name;
+
+
+                }
+
+            }
+        
+        }
+	}
+}
